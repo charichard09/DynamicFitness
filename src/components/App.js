25c0ab743e707834e6af2116f4a03dddd18f0e89
Richard <charichard09@gmail.com>
2023-02-23 11:33:42 -0800

    add individual pages folders for each route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './Header';
-import MyProfile from '../pages/MyProfile';
-import CreateWorkout from '../pages/CreateWorkout';
-import StartWorkout from '../pages/StartWorkout';
-import LoginLogout from '../pages/LoginLogout';
-import Home from '../pages/Home';
+import MyProfile from '../pages/MyProfile/MyProfile';
+import CreateWorkout from '../pages/CreateWorkout/CreateWorkout';
+import StartWorkout from '../pages/StartWorkout/StartWorkout';
+import LoginLogout from '../pages/LoginLogout/LoginLogout';
+import Home from '../pages/Home/Home';
 import Footer from './Footer';
 
 function App() {
