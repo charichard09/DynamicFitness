068969fc85557d26e2d9838335951fd0ee29bd21
Richard <charichard09@gmail.com>
2023-02-24 09:03:42 -0800

    WIP: basic css styling to login/logout

--- src/pages/LoginLogout/LoginLogout.js
+++ src/pages/LoginLogout/LoginLogout.js
@@ -1,10 +1,39 @@
-function LoginLogout() {
+import React from 'react';
+import imageDumbbell from '../../assets/dumbbell.png';
 
-  return(
-    <div style={{ "backgroundColor": "RGB(255, 205, 41)", "height": "100vh" }}>
-      <h3 style={{margin: 0}}>Login/Logout</h3>
+function LoginLogout() {
+  return (
+    <div style={{ backgroundColor: "RGB(255, 205, 41)" }}>
+      <div style={{ height: "50vh", position: "relative" }}>
+        <div style={{ 
+          backgroundImage: `url(${imageDumbbell})`, 
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "absolute",
+          top: "30%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "60%", /* adjust as needed */
+          height: "50%" /* adjust as needed */
+        }}>
+        </div>
+      </div>
+      
+      <form>
+        <label>Username</label>
+        <br/>
+        <input type="text" name="username" />
+        <br/>
+        <label>Password</label>
+        <br/>
+        <input type="text" name="password" />
+        <br/>
+        <button>Login</button>
+      </form>
     </div>
   );
 }
+
 
 export default LoginLogout;
