ac0c407fd5a353f29c365b4d54629aff1f5560b9
Richard <charichard09@gmail.com>
2023-03-07 07:27:12 -0800

    add queryable exercise function

--- src/pages/CreateWorkout/useGenerateWorkout.js
+++ src/pages/CreateWorkout/useGenerateWorkout.js
@@ -1,14 +1,39 @@
 import React, { useEffect } from 'react';
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { query, where } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
 
 function useGenerateWorkout(workout) {
   const [generatedWorkout, setGeneratedWorkout] = React.useState(workout);
 
-  useEffect(() => {
-    setGeneratedWorkout(prev => ({ ...prev, test: "test" }));
-  }
-  , []);
+  const firestore = useFirestore();
+  const exerciseCollection = collection(firestore, "exercises");
+  const exerciseQuery = query(exerciseCollection, where("difficulty", "==", "medium"));
 
-  return generatedWorkout;
+  console.log(exerciseQuery);
+  const { status, data } = useFirestoreCollectionData(exerciseQuery, { idField: "id" });
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  return(
+    <React.Fragment>
+      <ul>
+        {data.map(exercise => (
+          <li key={exercise.id}>{exercise.name}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
+
+
+  // useEffect(() => {
+  //   setGeneratedWorkout(prev => ({ ...prev, test: "test" }));
+  // }
+  // , []);
+
+  // return generatedWorkout;
 }
 
 export default useGenerateWorkout;
