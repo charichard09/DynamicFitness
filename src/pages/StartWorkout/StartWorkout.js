a2c95c250c3d0437cb4250a418ef5edc1e83647a
Richard <charichard09@gmail.com>
2023-03-08 16:55:45 -0800

    add logic to display specific workout

--- src/pages/StartWorkout/StartWorkout.js
+++ src/pages/StartWorkout/StartWorkout.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import SelectWorkoutDropdown from './SelectWorkoutDropdown';
+import SelectSplitDropdown from './SelectSplitDropdown';
+import Stopwatch from './Stopwatch';
+import WorkoutForm from './WorkoutForm';
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
 import { query, where } from 'firebase/firestore';
 import { collection } from 'firebase/firestore';
 import { auth } from '../../firebase';
-import SelectSplitDropdown from './SelectSplitDropdown';
 
 function StartWorkout() {
   const [nameOfWorkout, setNameOfWorkout] = useState(null);
-  const [splitOfWorkout, setSplitOfWorkout] = useState(null);
-  const [splitDropdownHidden, setSplitDropdownHidden] = useState(true);
+  const [allSplitsOfWorkout, setAllSplitsOfWorkout] = useState(null);
   const [workout, setWorkout] = useState(null);
+  const [splitOfWorkout, setSplitOfWorkout] = useState(null);
+  const [showWorkoutForm, setShowWorkoutForm] = useState(false);
 
   // get collection of workouts from db where [workoutdb].userId === auth.currentUser.uid
   const firestore = useFirestore();
@@ -18,40 +21,28 @@ function StartWorkout() {
   const userCreatedWorkoutsQuery = query(userCreatedWorkoutsCollection, where('userId', '==', auth.currentUser.uid));
 
   const { data } = useFirestoreCollectionData(userCreatedWorkoutsQuery, { idField: 'id' });
-  // let tempObjectOfWorkouts = {};
-  // if (data) {
-  //   data.forEach(workout => {
-  //     tempObjectOfWorkouts[workout.name] = workout;
-  //   });
-  // }
 
   useEffect(() => {
-    setSplitDropdownHidden(false);
     console.log("workout: ", workout);
     if (workout) {
-      setSplitOfWorkout(workout.split);
+      setAllSplitsOfWorkout(workout.split);
     }
-    console.log("splitOfWorkout: ", splitOfWorkout);
-  }, [nameOfWorkout, workout, splitOfWorkout]);
+    console.log("allSplitsOfWorkout: ", allSplitsOfWorkout);
+  }, [nameOfWorkout, workout, allSplitsOfWorkout]);
 
   // display dropdown with list of matching workouts.names
   // after a user clicks 'select' on a workout.name, next display dropdown of that workout objects.split (A, B, C).
   function handleSelectingWorkout(selectedWorkout) {
-    console.log("selected workout:", selectedWorkout);
-    console.log(data)
     setNameOfWorkout(selectedWorkout);
 
     // find matching name to workout object
     setWorkout(data.find(workout => workout.name === selectedWorkout));
-    
   }
 
   function handleSelectingSplit (selectedSplit) {
-    console.log("selected split:", selectedSplit);
     setSplitOfWorkout(selectedSplit);
+    setShowWorkoutForm(true);
   }
-
-  console.log("splitOfWorkout: ", splitOfWorkout);
 
   // after a user clicks 'select' on a split, next display that split's exercises with the ability to see, edit sets and reps and other details
 
@@ -62,7 +53,9 @@ function StartWorkout() {
       {/* {data ? data.map(workout => (<p key={workout.id}>{workout.split.ADay[0].name}</p>)) : null} */}
       <h3 style={{margin: 0}}>Select a workout:</h3>
       <SelectWorkoutDropdown allWorkouts={data} onSelectingWorkout={handleSelectingWorkout}/>
-      {splitOfWorkout ? <SelectSplitDropdown splits={splitOfWorkout} onSelectingSplit={handleSelectingSplit}/> : null }
+      {nameOfWorkout ? <SelectSplitDropdown splits={allSplitsOfWorkout} onSelectingSplit={handleSelectingSplit}/> : null }
+      {showWorkoutForm ? <WorkoutForm /> : null}
+      {showWorkoutForm ? <Stopwatch /> : null}  
     </div>
   );
 }
