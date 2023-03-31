b4ceaaa2509b5def2e1c5e0088c8cfb54ae3de7e
Richard <charichard09@gmail.com>
2023-03-31 15:19:31 -0700

    fix duplicate workoutName bug and removed import ionic structure to get scroll bar back

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,25 @@ import Home from '../pages/Home/Home';
 import Footer from './Footer';
 import { getFirestore } from 'firebase/firestore';
 import { useFirebaseApp, FirestoreProvider, AuthProvider } from 'reactfire';
-import { auth } from '../firebase';
-import '@ionic/react/css/core.css'
+import { auth } from '../firebase'
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import { setupIonicReact } from '@ionic/react';
+
+setupIonicReact();
 
 function App() {
   const firestoreInstance = getFirestore(useFirebaseApp());
