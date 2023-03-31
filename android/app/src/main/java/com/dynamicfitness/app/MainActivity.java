a3c2adbbfd3417c62c25e705b39b4edc94b3c44b
Richard <charichard09@gmail.com>
2023-03-31 11:34:16 -0700

    insall capacitor cli

---
+++ android/app/src/main/java/com/dynamicfitness/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dynamicfitness.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dynamicfitness.app',
+  appName: 'Dynamic Fitness',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
