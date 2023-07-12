70e2e104db58ff9e32ebdb2356c2a5f3701ccc39
Richard <charichard09@gmail.com>
2023-07-12 12:37:55 -0700

    basic tailwind css

--- src/pages/Calories/Calories.js
+++ src/pages/Calories/Calories.js
@@ -100,42 +100,46 @@ export default function Calories() {
   return (
     <div style={{ "backgroundColor": "RGB(255, 205, 41)", "height": "100vh" }}>
       {/* <h3 style={{margin: 0}}>Tracking Calories For {`${day}, ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`}</h3> */}
-      <label htmlFor="date">Date:</label>
-      <br />
-      <input type="date" id="date" defaultValue={submitDate} placeholder={submitDate} onChange={handleChangeDate} />
-      <br />
-      <form onSubmit={(e) => {handleTrack(e)}}>
-        <label htmlFor="food">Food:</label>
+      <div className="pl-5 py-3"> 
+        <label htmlFor="date">Date:</label>
         <br />
-        <input type="text" id="food" defaultValue="" placeholder="" />
+        <input type="date" id="date" defaultValue={submitDate} placeholder={submitDate} onChange={handleChangeDate} />
         <br />
-        <label htmlFor="calories">Calories:</label>
-        <br />
-        <input type="number" id="calories" defaultValue={0} />
-        <br />
-        {/* <input hidden type="text" id="date" defaultValue={`${day}, ${date.getMonth()}, ${date.getFullYear()}`} />
-        <br /> */}
-        <button type="submit" className="bg-slate-900 hover:bg-slate-700 text-white font-bold py-1 px-4 mr-3 mt-4 mb-1 ml-3 rounded">Track</button>
-      </form>
-      
-      <h3>Daily Log:</h3>
-      <div className="flex flex-col">
-        {
-          calories.map((entry, index) => {
-            return (
-              <div key={index} className="m-3 border-4 border-black bg-white pl-3 p-1 pb-4 mt-2 mb-5 flex-col h-auto" >
-                <h4 className="text-2x1">{entry.food}</h4>
-                <h4 className="text-2x1">{entry.calories} Calories</h4>
-              </div>
-            )  
-          })
-        }
+        <form onSubmit={(e) => {handleTrack(e)}}>
+          <label htmlFor="food">Food:</label>
+          <br />
+          <input type="text" id="food" defaultValue="" placeholder="" />
+          <br />
+          <label htmlFor="calories">Calories:</label>
+          <br />
+          <input type="number" id="calories" defaultValue={0} />
+          <br />
+          {/* <input hidden type="text" id="date" defaultValue={`${day}, ${date.getMonth()}, ${date.getFullYear()}`} />
+          <br /> */}
+          <button type="submit" className="bg-slate-900 hover:bg-slate-700 text-white font-bold py-1 px-4 mr-3 mt-4 mb-1 ml-3 rounded">Track</button>
+        </form>
       </div>
+      
+      <div className="pl-5"> 
+        <h3>Daily Log:</h3>
+        <div className="flex flex-col">
+          {
+            calories.map((entry, index) => {
+              return (
+                <div key={index} className="m-3 border-4 border-black bg-white pl-3 p-1 pb-4 mt-2 mb-5 flex-col h-auto" >
+                  <h4 className="text-2x1">{entry.food}</h4>
+                  <h4 className="text-2x1">{entry.calories} Calories</h4>
+                </div>
+              )
+            })
+          }
+        </div>
 
-      <h3>Current Total Calorie Intake:</h3>
-      <div className="flex flex-col">
-        {dailyCalories.totalCalories}
-        <button type="button" onClick={() => handleSubmitDay()} className="bg-slate-900 hover:bg-slate-700 text-white font-bold py-1 px-4 mr-3 mt-4 mb-1 ml-3 rounded">Submit Day</button>
+        <h3>Current Total Calorie Intake:</h3>
+        <div className="flex flex-col">
+          {dailyCalories.totalCalories}
+          <button type="button" onClick={() => handleSubmitDay()} className="bg-slate-900 hover:bg-slate-700 text-white font-bold py-1 mr-3 mt-4 mb-1 ml-3 rounded">Submit Day</button>
+        </div>
       </div>
     </div>
   );
